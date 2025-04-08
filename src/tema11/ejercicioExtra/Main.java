af5b5d73ffecda66c1f568c1ca120bae65582e25
antoniomoreno <amorenoa28@educarex.es>
2025-04-08 21:31:49 +0200

    tema11-7 de abril

--- src/tema11/ejercicioExtra/Main.java
+++ src/tema11/ejercicioExtra/Main.java
@@ -1,5 +1,7 @@
 package tema11.ejercicioExtra;
 
+import java.util.Scanner;
+
 /**
  * Construir un programa que permita gestionar la tabla employees de la bd Classicmodels.
  * Hay que descargar la tabla en un ResulSet, leer cada linea e instancias un objeto Empleado y
@@ -10,4 +12,37 @@ package tema11.ejercicioExtra;
  */
 
 public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        GestionE ge = new GestionE();
+
+        // Insertar un empleado
+        int n = ge.obtenerUltimoId();
+        ge.insertarEmpleado(new Empleado(n, "Ortiz", "Ramon", "x110",
+                "email@suarezf.es", "1", 1002, "Sales Rep"));
+
+        System.out.println(ge.buscarEmpleado(1166));
+
+        ge.eliminarEmpleado(1702);
+
+
+        ge.modificarEmpleado(ge.buscarEmpleado(n));
+
+
+        // Eliminar un empleado
+
+        // Modificar un empleadoç
+
+        // Buscar un empleado
+
+        // Listar todos los empleados
+        // Listar empleados por nombre
+        // Listar empleados por apellidos
+        // Listar empleados por oficina
+        // Listar empleados por jefe
+
+
+    }
+
 }
